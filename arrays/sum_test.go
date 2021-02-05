07d1563456220533c34709dc2a31a014f5c0e727
foyez <foyezar@gmail.com>
2021-02-06 01:13:53 +0600

    implement slice instead of array

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -5,12 +5,36 @@ import (
 )
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+	// assertCorrectMessage := func(t *testing.TB, got, want int) {
+	// 	t.Helper()
 
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+	// 	if got != want {
+	// 		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	// 	}
+	// }
+	
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		// numbers := [5]int{1, 2, 3, 4, 5} // numbers := [...]int{1, 2, 3, 4, 5} - array
+		numbers := []int{1, 2, 3, 4, 5}
+
+		got := Sum(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
 }
