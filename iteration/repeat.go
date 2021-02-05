712050b58e718b2b4d35b8b7962c1468f4cb2c4a
foyez <foyezar@gmail.com>
2021-02-06 00:09:30 +0600

    add benchmark

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,10 +1,12 @@
 package iteration
 
+const repeatedCount = 5
+
 // Repeat takes one string and returns repeated string
 func Repeat(character string) string {
 	var repeated string
 
-	for i := 0; i < 5; i++ {
+	for i := 0; i < repeatedCount; i++ {
 		repeated += character
 	}
 
