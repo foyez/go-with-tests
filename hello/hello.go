e6cc33532a5342de9734aceaa996011a96063729
foyez <foyezar@gmail.com>
2021-02-05 21:32:18 +0600

    add functionality for other language

--- hello/hello.go
+++ hello/hello.go
@@ -2,17 +2,34 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-// Hello takes one string and returns the greeting string
-func Hello(name string) string {
+// Hello takes two string and returns the greeting string
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	return englishHelloPrefix + name
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
