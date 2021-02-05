4c239cd60746caeb4f0eb51167dd981020ed7ad6
foyez <foyezar@gmail.com>
2021-02-06 00:26:22 +0600

    add ExampleRepeat()

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,9 +1,9 @@
 package iteration
 
-const repeatedCount = 5
+// const repeatedCount = 5
 
-// Repeat takes one string and returns repeated string
-func Repeat(character string) string {
+// Repeat takes two params and returns repeated string
+func Repeat(character string, repeatedCount int) string {
 	var repeated string
 
 	for i := 0; i < repeatedCount; i++ {
