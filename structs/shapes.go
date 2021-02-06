6af06981ecfd7be6cb8a0b8810a369e4e14a4058
foyez <foyezar@gmail.com>
2021-02-06 13:35:44 +0600

    ADD: method for rectangle & circle

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,11 +1,35 @@
 package structs
 
+import "math"
+
+// Circle has Radius property of a circle
+type Circle struct {
+	Radius float64
+}
+
+// Area returns the area of the circle
+// method - func (receiverName ReceiverType) MethodName(args)
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+// Rectangle has the dimensions of a rectangle
+type Rectangle struct {
+	Width float64
+	Height float64
+}
+
+// Area returns the area of the rectangle
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
 // Perimeter takes two float64 and returns perimeter in float64
-func Perimeter(height, width float64) float64 {
-	return 2 * (height + width)
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
 
 // Area takes height, width and returns area
-func Area(height, width float64) float64 {
-	return height * width
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
 }
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -1,9 +1,13 @@
 package structs
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -12,10 +16,46 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(10.0, 10.0)
-	want := 100.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := rectangle.Area()
+		want := 100.0
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+				t.Errorf("got %g want %g", got, want)
+		}
+	})
+
+}
+
+func ExamplePerimeter() {
+	rectangle := Rectangle{10.0, 10.0}
+	perimeter := Perimeter(rectangle)
+	fmt.Println(perimeter)
+	// Output: 40
+}
+
+func ExampleArea() {
+	rectangle := Rectangle{10.0, 10.0}
+	area := rectangle.Area()
+	fmt.Println(area)
+	// Output: 100
+}
+
+func ExampleArea_second() {
+	circle := Circle{10.0}
+	area := circle.Area()
+	fmt.Println(area)
+	// Output: 314.1592653589793
 }
