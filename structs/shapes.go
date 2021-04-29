422ccbe4bdaab4598798ada196038abb4af057dc
foyez <foyezar@gmail.com>
2021-04-29 21:17:38 +0600

    add table driven tests

--- structs/shapes.go
+++ structs/shapes.go
@@ -28,6 +28,17 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
+// Triangle has base and height property of a triangle
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+// Area return the area of the triangle
+func (t Triangle) Area() float64 {
+	return .5 * t.Base * t.Height
+}
+
 // Perimeter takes rectangle type and returns perimeter
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
