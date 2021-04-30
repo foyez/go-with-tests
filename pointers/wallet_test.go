30e487874d632cd50550060963947b483ea0ab3a
foyez <foyezar@gmail.com>
2021-04-30 22:16:59 +0600

    add Delete method in dictionary

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -15,8 +15,10 @@ func TestWallet(t *testing.T) {
 
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
+
 		assertBalance(t, wallet, Bitcoin(10))
+		assertNotError(t, err)
 	})
 
 	t.Run("Withdraw insufficient funds", func(t *testing.T) {
@@ -40,11 +42,19 @@ func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
 	}
 }
 
+func assertNotError(t testing.TB, got error) {
+	t.Helper()
+
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
+}
+
 func assertError(t testing.TB, got error, want error) {
 	t.Helper()
 
 	if got == nil {
-		t.Error("didn't get an error but wanted one")
+		t.Fatal("didn't get an error but wanted one")
 	}
 
 	if got != want {
